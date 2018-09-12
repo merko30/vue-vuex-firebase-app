21deb8f4b6c89687e7d3b002032635f3137b1c1e
merko30 <32275735+merko30@users.noreply.github.com>
2018-09-12 08:32:52 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 export default {
-    apiKey: "AIzaSyC1rux45_A4fehWT2XMj2bQhkyK5p5jTVI",
-    authDomain: "vuefire-e671e.firebaseapp.com",
-    databaseURL: "https://vuefire-e671e.firebaseio.com/",
-    projectId: "vuefire-e671e",
-    storageBucket: "gs://vuefire-e671e.appspot.com"
+    apiKey: "API KEY",
+    authDomain: "AUTH DOMAIN",
+    databaseURL: "DATABASE URL",
+    projectId: "PROJECT ID",
+    storageBucket: "STORAGE BUCKET"
 };
